50be35f22015bb6a31d1889b25985bae0712abeb
JudeRanidu <juderanidu96@gmail.com>
2020-05-20 18:47:35 +0530

    Email customization

--- room_booking.js
+++ room_booking.js
@@ -1,9 +1,9 @@
-var formCreator = '<form action="rooms.php" method="POST" name="book_form" onsubmit="return confirmBooking();"><div><label>Name</label><input type="text" name="name" required></div<div><label>Email</label><input type="email" name="email" required></div><div><label>Telephone</label><input type="tel" name="tel" pattern="0[0-9]{9}" required></div><div><div><button type="submit" >Confirm</button></div><div> <button type="button" onclick="Alert.okCancel()">Cancel</button></div></div></form>';
+var formCreator = '<form action="rooms.php" method="POST" name="book_form" onsubmit="return confirmBooking();"><div><label>Name</label><input type="text" name="name" required></div<div><label>Email</label><input type="email" name="email" required></div><div><label>Telephone</label><input type="tel" name="tel" pattern="0[0-9]{9}" required></div><div><div><button name="confirm_btn" type="submit" >Confirm</button></div><div> <button name="cancel_btn" type="button" onclick="Alert.okCancel()">Cancel</button></div></div></form>';
 var unavailability_msg = 'Sorry...We are unable to cater your booking at the moment due to unavailability of rooms';
 var availability_msg = 'Your rooms are available..You can proceed and confirm the booking'; 
 var ok_html = '<div><button onclick="Alert.okCancel()">OK</button></div>';
 //var confirm_html = '<div><button type="submit" onclick="confirmBooking()">Confirm</button></div><div> <button onclick="Alert.okCancel()">Cancel</button></div>';
-var proceed_html = '<div><button onclick="Alert.render(formCreator,\' \')">Proceed</button></div>';
+var proceed_html = '<div><button name="proceed_btn" onclick="Alert.render(formCreator,\' \')">Proceed</button></div>';
 var check_msg = 'Check-in and check-out dates do not match...No of rooms cannot be zero';
 var ok_1_html = '<div><button onclick="Alert.ok()">OK</button></div>';
 var mail_html = 'Your Booking is placed...Confirmaton email is sent to you';
@@ -109,7 +109,9 @@ function confirmBooking(){
   var telephone = document.forms["book_form"]["tel"].value;
   var checkIn = document.forms["check_form"]["check-in"].value;
   var checkOut = document.forms["check_form"]["check-out"].value;
-  
+  var room_type = document.forms["check_form"]["room_type"].value;
+  var package = document.forms["check_form"]["package"].value;
+
   var rooms = "";
   if ((d_rooms.length==0) && (s_rooms.length!=0)){
     rooms = s_rooms.join()
@@ -118,7 +120,7 @@ function confirmBooking(){
     rooms = d_rooms.join()
   }
   if ((d_rooms.length!=0) && (s_rooms.length!=0)){
-    rooms = d_rooms.join()+" ,"+s_rooms.join()
+    rooms = d_rooms.join()+","+s_rooms.join()
   }
   
   db.collection("room-bookings").add({
@@ -128,7 +130,7 @@ function confirmBooking(){
     check_in:checkIn,
     check_out:checkOut,
     rooms:rooms,
-    package:document.forms["check_form"]["package"].value
+    package:package
   })
 
   if (d_rooms.length!=0){
@@ -158,12 +160,12 @@ function confirmBooking(){
     }
   }
   
-  function sendMail(msg_body) {
+  function sendMail(msg_body,email) {
     Email.send({
     Host: "smtp.gmail.com",
     Username : "hotelpearlmis@gmail.com",
     Password : "pearl#123",
-    To : 'ranidusjcrox@gmail.com',
+    To : email,
     From : "hotelpearlmis@gmail.com",
     Subject : "Booking Confirmation",
     Body : msg_body,
@@ -172,11 +174,20 @@ function confirmBooking(){
     );
   }
 
-  var double = "Double-Bed Rooms : "+d_rooms.join();
-  var single = "Single-Bed Rooms : "+s_rooms.join();
-  var msg_body = "The following rooms have been booked \nfor you from "+checkIn+" to "+checkOut+" \n" +double+" \n"+single;
+  var double = d_rooms.join();
+  var single = s_rooms.join();
+
+  var msg1 = '<html><body><div style="box-sizing: border-box; font-family: sans-serif; width: 100%; height:fit-content; background-color: black; padding-top: 20px; padding-bottom: 20px; text-align: center;"><div style="font-size: 17pt; box-sizing: border-box; width: 95%; margin: auto; height:fit-content; background-color:dodgerblue;color: ghostwhite; padding: 15px 15px; ">';
+  var msg2 = 'Hotel Pearl - Booking Confirmation</div><div style="box-sizing: border-box; width: 95%; height: fit-content; background-color: white; margin: auto; padding-bottom: 20px;"><table style="box-sizing: border-box; width:95%; height: fit-content; margin: auto; padding-top: 20px; font-size: 13pt; text-align: center;"><tr>';
+  var msg3 = '<td style="box-sizing: border-box;background-color:lightslategrey; padding: 10px 10px;">Check-In Date</td><td style="box-sizing: border-box;background-color: lightslategrey; padding: 10px 10px;">'+checkIn+'</td></tr><tr><td style="box-sizing: border-box;background-color: lightslategrey; padding: 10px 10px;">';
+  var msg4 = 'Check-Out Date</td><td style="box-sizing: border-box;background-color: lightslategrey; padding: 10px 10px;">'+checkOut+'</td></tr>';
+  var msg5 = '<tr><td style="box-sizing: border-box;background-color: lightslategrey; padding: 10px 10px;">Package Type</td><td style="box-sizing: border-box;background-color: lightslategrey; padding: 10px 10px;">'+package+'</td></tr>';
+  var msg6 = '<tr><td style="box-sizing: border-box;background-color: lightslategrey; padding: 10px 10px;">Room Type</td><td style="box-sizing: border-box;background-color: lightslategrey; padding: 10px 10px;">'+room_type+'</td></tr>';
+  var msg7 = '<tr><td style="box-sizing: border-box;background-color: lightslategrey; padding: 10px 10px;">Double Bedroom-Room Nos</td><td style="box-sizing: border-box;background-color: lightslategrey; padding: 10px 10px;">'+double+'</td></tr><tr><td style="box-sizing: border-box;background-color: lightslategrey; padding: 10px 10px;">Single Bedroom-Room Nos</td><td style="box-sizing: border-box;background-color: lightslategrey; padding: 10px 10px;">'+single+'</td></tr></table></div></div></body></html>';
+
+  var msg_body = msg1+msg2+msg3+msg4+msg5+msg6+msg7;
   
-  sendMail(msg_body);
+  sendMail(msg_body,email);
 
   return false;
 }
